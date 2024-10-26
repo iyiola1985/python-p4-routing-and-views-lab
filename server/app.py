f120004c65634240906467928913942bc9f23656
iyiola olusola ebenezer <sholaiyiola1966@gmail.com>
2024-10-27 01:39:19 +1100

    new one

--- server/app.py
+++ server/app.py
@@ -1,8 +1,43 @@
-#!/usr/bin/env python3
-
-from flask import Flask
+from flask import Flask, jsonify, render_template_string
 
 app = Flask(__name__)
 
+# Index route
+@app.route('/')
+def index():
+    return render_template_string('''
+        <h1>Python Operations with Flask Routing and Views</h1>
+    ''')
+
+# Print string route
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)  # Print to console
+    return f'<h2>Printed: {parameter}</h2>'
+
+# Count route
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    numbers = '<br>'.join(str(i) for i in range(parameter + 1))  # Create a string of numbers
+    return f'<h2>Counting to {parameter}:</h2><pre>{numbers}</pre>'
+
+# Math route
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2 if num2 != 0 else 'Division by zero error'
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return 'Invalid operation!', 400
+    
+    return f'<h2>Result of {num1} {operation} {num2} = {result}</h2>'
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
